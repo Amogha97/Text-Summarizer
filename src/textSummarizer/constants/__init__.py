00ea791c95c839475049f40d5ac7b05c33f0c35b
Amogha Gadde <amoghagadde@Amoghas-MacBook-Pro.local>
2024-08-03 12:16:19 -0400

    data ingestion notebook added

---
+++ src/textSummarizer/constants/__init__.py
@@ -0,0 +1,11 @@
+# from pathlib import Path
+
+# CONFIG_FILE_PATH = Path("config/config.yaml")
+# PARAMS_FILE_PATH = Path("params.yaml")
+
+
+from pathlib import Path
+
+# Ensure the absolute path is correct
+CONFIG_FILE_PATH = Path("/Users/amoghagadde/Desktop/Amogha/Projects/Data_Science/Text-Summarizer/config/config.yaml")
+PARAMS_FILE_PATH = Path("/Users/amoghagadde/Desktop/Amogha/Projects/Data_Science/Text-Summarizer/params.yaml")
